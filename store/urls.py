ee1a2ef37b8e39cb5da49fb2ea54db81f3cf8e80
yordanossole <yordanos.sole@gmail.com>
2025-05-05 20:51:57 +0300

    urls.py updated to function based

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:id>', views.ProductDetail.as_view()),
-    path('categories/', views.CategoryList.as_view()),
-    path('categories/<int:pk>', views.CategoryDetail.as_view(), name='category-detail'),
+    path('categories/', views.category_list),
+    path('categories/<int:pk>', views.category_detail, name='category-detail'),
+    path('products/', views.product_list),
+    path('products/<int:pk>', views.product_detail),
 ]
+
+# urlpatterns = [
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:id>', views.ProductDetail.as_view()),
+#     path('categories/', views.CategoryList.as_view()),
+#     path('categories/<int:pk>', views.CategoryDetail.as_view(), name='category-detail'),
+# ]
